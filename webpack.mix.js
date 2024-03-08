a70bd98624780daad6d253cc685c7f67470f692c
Federico <fede.toldo96@gmail.com>
2024-03-08 15:32:07 +0100

    remove gsap

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,7 +4,6 @@ let mix = require('laravel-mix');
 let minifier = require('minifier');
 
 mix.sass('assets/scss/style.scss', 'style.css');
-mix.combine(['assets/js/script.js'], 'script.js');
 mix.minify(['script.js']);
 
 mix.webpackConfig({

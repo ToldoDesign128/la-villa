0a0e8f1c8b5c4bd637d716830c5fbd0279ff1a73
Federico <fede.toldo96@gmail.com>
2024-03-08 10:05:45 +0100

    aggiornamento

--- page-camere.php
+++ page-camere.php
@@ -7,7 +7,7 @@
             <h2>Le Camere</h2>
         </div>
         <div class="camere__hero__image">
-            <img loading="lazy" src="https://lavillapinarella.it/wp-content/uploads/2024/03/camere_hero.jpg" alt="">
+            <img loading="lazy" src="<?php echo get_template_directory_uri() ?>/assets/image/camere/camere_hero.jpg" alt="">
         </div>
     </section>
 
@@ -21,7 +21,7 @@
                     <p>Con la camera invece è compreso la pulizia giornaliera, la biancheria, il wi-fi, la tv e l’aria condizionata ed una convenzione per l’ombrellone alla spiaggia e due lettini a 10 euro al giorno.</p>
                 </div>
                 <div class="camere__poster__container__block__image">
-                    <img loading="lazy" src="https://lavillapinarella.it/wp-content/uploads/2024/03/camere_2-scaled.jpg" alt="">
+                    <img loading="lazy" src="<?php echo get_template_directory_uri() ?>/assets/image/camere/camere_2.jpg" alt="">
                 </div>
             </div>
         </div>
@@ -44,7 +44,7 @@
                     <p>Un balcone che affaccia direttamente in pineta a 50 metri dal mare, si può sentire il rumore delle onde infrangersi sulla batigia e il cinguettio degli uccellini al risveglio la mattina.</p>
                 </div>
                 <div class="camere__gallery__container__block__image">
-                    <img loading="lazy" src="https://lavillapinarella.it/wp-content/uploads/2024/03/camere_pineta.jpg" alt="">
+                    <img loading="lazy" src="<?php echo get_template_directory_uri() ?>/assets/image/camere/camere_pineta.jpg" alt="">
                 </div>
             </div>
         </div>
@@ -55,7 +55,7 @@
         <div class="container camere__gallery2__container">
             <div class="row camere__gallery2__container__block">
                 <div class="camere__gallery2__container__block__image">
-                    <img loading="lazy" src="https://lavillapinarella.it/wp-content/uploads/2024/03/camera_viale.jpg" alt="">
+                    <img loading="lazy" src="<?php echo get_template_directory_uri() ?>/assets/image/camere/camera_viale.jpg" alt="">
                 </div>
                 <div class="camere__gallery2__container__block__text">
                     <h3>Lato Viale Italia</h3>
--- page-eventi.php
+++ page-eventi.php
@@ -1,56 +1,57 @@
 <?php get_header(); ?>
 
-<main class="camere">
+<main class="eventi">
     <!-- Hero -->
-    <section class="camere__hero">
-        <div class="row camere__hero__title">
+    <section class="eventi__hero">
+        <div class="row eventi__hero__title">
             <h2>I nostri Eventi</h2>
         </div>
-        <div class="camere__hero__image">
-            <img loading=lazy src="https://lavillapinarella.it/wp-content/uploads/2023/03/foto_camere-9_9_11zon-scaled.jpg" alt="">
+        <div class="eventi__hero__image">
+            <img loading=lazy src="<?php echo get_template_directory_uri() ?>/assets/image/eventi/hero.jpg" alt="">
         </div>
     </section>
 
-    <!-- gallery2 eventi-->
-    <section class="camere__gallery2">
-        <div class="container camere__gallery2__container">
-            <div class="row camere__gallery2__container__block">
-                <div class="camere__gallery2__container__block__image">
-                    <?php get_template_part('template-parts/loop-camere2'); ?>
-                </div>
-                <div class="camere__gallery2__container__block__text">
+    <!-- gallery1 eventi-->
+    <section class="eventi__gallery">
+        <div class="container eventi__gallery__container">
+            <div class="row eventi__gallery__container__block">
+                <div class="eventi__gallery__container__block__text">
                     <h3>La musica in pineta</h3>
                     <p>Alla Villa organizziamo concerti, invitiamo amici e musicisti della zona, ci mettiamo seduti in pineta, sulla terra e ci lasciamo coinvolgere dalla musica, i concerti finiscono presto, alle 23:30 la musica si stacca, chi vuole può fare una passeggiata a vedere il mare c'è chi invece preferisce restare a fare due chiacchiere con gli amici, ma poi dritti a nanna perchè domattina ci incontriamo tutti in spiaggia.</p>
+
+                </div>
+                <div class="eventi__gallery__container__block__image">
+                    <img loading="lazy" src="<?php echo get_template_directory_uri() ?>/assets/image/eventi/musica.jpg" alt="">
                 </div>
             </div>
         </div>
     </section>
 
     <!-- gallery2 eventi-->
-    <section class="camere__gallery2">
-        <div class="container camere__gallery2__container">
-            <div class="row camere__gallery2__container__block">
-                <div class="camere__gallery2__container__block__image">
-                    <?php get_template_part('template-parts/loop-camere2'); ?>
+    <section class="eventi__gallery2">
+        <div class="container eventi__gallery2__container">
+            <div class="row eventi__gallery2__container__block">
+                <div class="eventi__gallery2__container__block__image">
+                    <img loading="lazy" src="<?php echo get_template_directory_uri() ?>/assets/image/eventi/yoga.jpg" alt="">
                 </div>
-                <div class="camere__gallery2__container__block__text">
-                    <h3>La musica in pineta</h3>
-                    <p>Alla Villa organizziamo concerti, invitiamo amici e musicisti della zona, ci mettiamo seduti in pineta, sulla terra e ci lasciamo coinvolgere dalla musica, i concerti finiscono presto, alle 23:30 la musica si stacca, chi vuole può fare una passeggiata a vedere il mare c'è chi invece preferisce restare a fare due chiacchiere con gli amici, ma poi dritti a nanna perchè domattina ci incontriamo tutti in spiaggia.</p>
+                <div class="eventi__gallery2__container__block__text">
+                    <h3>Lo yoga alla mattina</h3>
+                    <p>Ogni tanto ci piace anche svegliarci presto, la pineta alle prime luci del mattino regala uno spettacolo incredibile e si può fare anche lo yoga! é gratis e perfetto anche per chi non ha mai provato, dopo però prima della spiaggia, ci facciamo una bella colazione in mezzo al cinguettio degli uccelini.</p>
                 </div>
             </div>
         </div>
     </section>
 
     <!-- gallery3 eventi-->
-    <section class="camere__gallery">
-        <div class="container camere__gallery__container">
-            <div class="row camere__gallery__container__block">
-                <div class="camere__gallery__container__block__text">
+    <section class="eventi__gallery3">
+        <div class="container eventi__gallery3__container">
+            <div class="row eventi__gallery3__container__block">
+                <div class="eventi__gallery3__container__block__text">
                     <h3>Arte e poesia</h3>
-                    <p>Ogni tanto ci piace anche svegliarci presto, la pineta alle prime luci del mattino regala uno spettacolo incredibile e si può fare anche lo yoga! é gratis e perfetto anche per chi non ha mai provato, dopo però prima della spiaggia, ci facciamo una bella colazione in mezzo al cinguettio degli uccelini.</p>
+                    <p>Illuminata di sera la pineta diventa un posto magico e proprio per questo è perfetta per ospitare non solo concerti ma letture, recitazioni e manifestazioni di vario genere.</p>
                 </div>
-                <div class="camere__gallery__container__block__image">
-                    <img loading="lazy" src="https://lavillapinarella.it/wp-content/uploads/2024/03/camere_pineta.jpg" alt="">
+                <div class="eventi__gallery3__container__block__image">
+                    <img loading="lazy" src="<?php echo get_template_directory_uri() ?>/assets/image/eventi/arte.jpg" alt="">
                 </div>
             </div>
         </div>
